1c72d902f2ec47a1818b561c636435b42d400582
“arkadijss” <80927090+arkadijss@users.noreply.github.com>
2023-12-04 21:57:21 +0000

    workaround for bag closing

--- scripts/collect_measurements.py
+++ scripts/collect_measurements.py
@@ -1,13 +1,19 @@
 import rospy
+
 from rosbag_utils.recording import BagRecorder
-from robot import RobotController
+from robot.controller import RobotController
 from conf.rosbag_conf import topics
 
 
 if __name__ == "__main__":
     rospy.init_node("collect_measurements")
-    bag_path = "bagfile.bag"
-    bag_recorder = BagRecorder(bag_path, topics)
-    bag_recorder.setup_subscribers()
-    robot_controller = RobotController()
-    robot_controller.start()
+
+    try:
+        bag_path = "bagfile.bag"
+        bag_recorder = BagRecorder(bag_path, topics)
+        bag_recorder.setup_subscribers()
+
+        robot_controller = RobotController()
+        robot_controller.start()
+    finally:
+        bag_recorder.close_bag()
